dfef6b7b165bab06d97e89edd99d50508eac47d4
oram1288 <cody.oram@keyin.com>
2024-07-16 22:51:36 -0230

    views creation, plus index

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const methodOverride = require("method-override");
+const app = express();
+const PORT = 3000;
+
+global.DEBUG = true;
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true })); // Important!
+app.use(methodOverride("_method")); // This too!
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { name: "Auston Mattthews" });
+});
+app.get("/about", (request, response) => {
+  response.render("about.ejs");
+});
+
+const playersRouter = require("./routes/players");
+app.use("/players", playersRouter);
+
+const loginsRouter = require("./routes/logins");
+app.use("/logins", loginsRouter);
+
+const usersRouter = require("./routes/users");
+app.use("/users", usersRouter);
+
+// anything beginning with "/api" will go into this
+const apiRouter = require("./routes/api");
+app.use("/api", apiRouter);
+
+app.use((req, res) => {
+  res.status(404).render("404");
+});
+
+app.listen(PORT, () => {
+  console.log(`Player app running on port ${PORT}.`);
+});
