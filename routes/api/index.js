1240701c46195364469ea75ce25bd5468b1646fb
oram1288 <cody.oram@keyin.com>
2024-07-24 16:04:53 -0230

    fixes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,10 @@
 var router = require("express").Router();
 
 if (DEBUG) {
-  console.log("ROUTE: /api/actors");
+  console.log("ROUTE: /api/players");
 }
 // http://localhost:3000/api/actors/
-const actorsRouter = require("./players");
-router.use("/actors", actorsRouter);
+const playersRouter = require("./players");
+router.use("/players", playersRouter);
 
 module.exports = router;
